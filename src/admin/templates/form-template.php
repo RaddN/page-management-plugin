3ba8f6b947ba13b1b30477c6d229f206682ed7d6
hossainraihan02 <hossainraihan02@gmail.com>
2025-02-26 07:44:04 +0600

    maked reusable

--- src/admin/templates/form-template.php
+++ src/admin/templates/form-template.php
@@ -4,178 +4,127 @@ if (!defined('ABSPATH')) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Handle form submissions
+    handle_form_submission();
+}
+
+function handle_form_submission() {
     if (isset($_POST['import_template'])) {
-        // Handle the import template form submission
-        $template_id = intval($_POST['existing_page']);
-        $template_post = get_post($template_id);
-        $template_content = $template_post->post_content;
+        import_template();
     } elseif (isset($_POST['create_page'])) {
-        // Handle the create page form submission
-        $parent_slug = sanitize_text_field($_POST['parent_slug']);
-        $page_name = sanitize_text_field($_POST['page_name']);
-        $page_slug = sanitize_text_field($_POST['page_slug']);
-        $template_title = sanitize_text_field($_POST['template_title']);
-
-        // Fetch the selected template's content
-        $template_id = intval($_POST['existing_page']);
-        $template_post = get_post($template_id);
-        $template_content = $template_post->post_content;
-
-        // Replace dynamic content placeholders with form data
-        foreach ($_POST as $key => $value) {
-            if (strpos($key, 'rdynamic_') === 0) {
-                $dynamic_name = substr($key, 9); // Remove 'rdynamic_' prefix
-                $template_content = preg_replace(
-                    '/\{\{\{rdynamic_content type=[\'"][^\'"]+[\'"] name=[\'"]' . preg_quote($dynamic_name, '/') . '[\'"] title=[\'"][^\'"]+[\'"]\}\}\}/',
-                    wp_kses_post($value),
-                    $template_content
-                );
-            }
-        }
-
-        // Handle loop content
-        if (preg_match('/\{\{\{rdynamic_content_loop_start\}\}\}(.*?)\{\{\{rdynamic_content_loop_ends\}\}\}/s', $template_content, $loop_match)) {
-            $loop_content = '';
-            $loop_template = $loop_match[1];
-            $loop_count = intval($_POST['loop_count']);
-
-            for ($i = 1; $i <= $loop_count; $i++) {
-                $current_loop_content = $loop_template;
-                foreach ($_POST as $key => $value) {
-                    if (strpos($key, 'rdynamic_' . $i . '_') === 0) {
-                        $dynamic_name = substr($key, 10 + strlen($i)); // Remove 'rdynamic_' and loop index prefix
-                        $current_loop_content = preg_replace(
-                            '/\{\{\{rdynamic_content type=[\'"][^\'"]+[\'"] name=[\'"]' . preg_quote($dynamic_name, '/') . '[\'"] title=[\'"][^\'"]+[\'"]\}\}\}/',
-                            wp_kses_post($value),
-                            $current_loop_content
-                        );
-                    }
-                }
-                $loop_content .= $current_loop_content;
-            }
-
-            $template_content = preg_replace(
-                '/\{\{\{rdynamic_content_loop_start\}\}\}(.*?)\{\{\{rdynamic_content_loop_ends\}\}\}/s',
-                $loop_content,
-                $template_content
-            );
-        }
-
-        // Create the new page
-        $new_page_id = wp_insert_post(array(
-            'post_title' => $page_name,
-            'post_name' => $page_slug,
-            'post_content' => $template_content,
-            'post_status' => 'publish',
-            'post_type' => 'page',
-            'post_parent' => !empty($parent_slug) ? get_page_by_path($parent_slug)->ID : 0,
-        ));
-
-        // Store form data in post meta
-        foreach ($_POST as $key => $value) {
-            if (strpos($key, 'rdynamic_') === 0) {
-                update_post_meta($new_page_id, $key, wp_kses_post($value));
-            }
-        }
-
-        // Set the rpage_category for the new page
-        update_post_meta($new_page_id, 'rpage_category', $template_title);
-        update_post_meta($new_page_id, 'rdynamic_template_id', $template_id);
-
-        // Redirect to the new page
-        wp_redirect(get_permalink($new_page_id));
-        exit;
+        create_page();
     } elseif (isset($_POST['update_page'])) {
-        // Handle the update page form submission
-        $page_id = intval($_POST['page_id']);
-        $page_name = sanitize_text_field($_POST['page_name']);
-        $page_slug = sanitize_text_field($_POST['page_slug']);
-        $template_title = sanitize_text_field($_POST['template_title']);
-
-        // Fetch the selected template's content
-        $template_id = intval($_POST['existing_page']);
-        $template_post = get_post($template_id);
-        $template_content = $template_post->post_content;
-
-        // Replace dynamic content placeholders with form data
-        foreach ($_POST as $key => $value) {
-            if (strpos($key, 'rdynamic_') === 0) {
-                $dynamic_name = substr($key, 9); // Remove 'rdynamic_' prefix
-                $template_content = preg_replace(
-                    '/\{\{\{rdynamic_content type=[\'"][^\'"]+[\'"] name=[\'"]' . preg_quote($dynamic_name, '/') . '[\'"] title=[\'"][^\'"]+[\'"]\}\}\}/',
-                    wp_kses_post($value),
-                    $template_content
-                );
-            }
-        }
-
-        // Handle loop content
-        if (preg_match('/\{\{\{rdynamic_content_loop_start\}\}\}(.*?)\{\{\{rdynamic_content_loop_ends\}\}\}/s', $template_content, $loop_match)) {
-            $loop_content = '';
-            $loop_template = $loop_match[1];
-            $loop_count = intval($_POST['loop_count']);
-
-            for ($i = 1; $i <= $loop_count; $i++) {
-                $current_loop_content = $loop_template;
-                foreach ($_POST as $key => $value) {
-                    if (strpos($key, 'rdynamic_' . $i . '_') === 0) {
-                        $dynamic_name = substr($key, 10 + strlen($i)); // Remove 'rdynamic_' and loop index prefix
-                        $current_loop_content = preg_replace(
-                            '/\{\{\{rdynamic_content type=[\'"][^\'"]+[\'"] name=[\'"]' . preg_quote($dynamic_name, '/') . '[\'"] title=[\'"][^\'"]+[\'"]\}\}\}/',
-                            wp_kses_post($value),
-                            $current_loop_content
-                        );
-                    }
-                }
-                $loop_content .= $current_loop_content;
-            }
-
-            $template_content = preg_replace(
-                '/\{\{\{rdynamic_content_loop_start\}\}\}(.*?)\{\{\{rdynamic_content_loop_ends\}\}\}/s',
-                $loop_content,
-                $template_content
-            );
-        }
-
-        // Update the existing page
-        wp_update_post(array(
-            'ID' => $page_id,
-            'post_title' => $page_name,
-            'post_name' => $page_slug,
-            'post_content' => $template_content,
-        ));
-
-        // Update form data in post meta
-        foreach ($_POST as $key => $value) {
-            if (strpos($key, 'rdynamic_') === 0) {
-                update_post_meta($page_id, $key, wp_kses_post($value));
-            }
-        }
-
-        // Update the rpage_category for the page
-        update_post_meta($page_id, 'rpage_category', $template_title);
-        update_post_meta($page_id, 'rdynamic_template_id', $template_id);
-
-        // Redirect to the updated page
-        wp_redirect(get_permalink($page_id));
-        exit;
+        update_page();
     } elseif (isset($_POST['delete_page'])) {
-        // Handle the delete page form submission
-        $page_id = intval($_POST['page_id']);
-
-        // Delete the page
-        wp_delete_post($page_id, true);
-
-        // Redirect to the admin page or any other page
-        wp_redirect(admin_url('edit.php?post_type=page'));
-        exit;
+        delete_page();
     }
 }
 
+function import_template() {
+    $template_id = intval($_POST['existing_page']);
+    $template_post = get_post($template_id);
+    $template_content = $template_post->post_content;
+    // Further processing for importing template...
+}
+
+function create_page() {
+    $parent_slug = sanitize_text_field($_POST['parent_slug']);
+    $page_name = sanitize_text_field($_POST['page_name']);
+    $page_slug = sanitize_text_field($_POST['page_slug']);
+    $template_content = fetch_template_content();
+
+    // Replace dynamic content placeholders
+    $template_content = replace_dynamic_content($template_content);
+
+    // Create the new page
+    $new_page_id = wp_insert_post(array(
+        'post_title' => $page_name,
+        'post_name' => $page_slug,
+        'post_content' => $template_content,
+        'post_status' => 'publish',
+        'post_type' => 'page',
+        'post_parent' => !empty($parent_slug) ? get_page_by_path($parent_slug)->ID : 0,
+    ));
+
+    // Store form data in post meta
+    store_post_meta($new_page_id);
+    
+    // Redirect to the new page
+    wp_redirect(get_permalink($new_page_id));
+    exit;
+}
+
+function update_page() {
+    $page_id = intval($_POST['page_id']);
+    $page_name = sanitize_text_field($_POST['page_name']);
+    $page_slug = sanitize_text_field($_POST['page_slug']);
+    $template_content = fetch_template_content();
+
+    // Replace dynamic content placeholders
+    $template_content = replace_dynamic_content($template_content);
+
+    // Update the existing page
+    wp_update_post(array(
+        'ID' => $page_id,
+        'post_title' => $page_name,
+        'post_name' => $page_slug,
+        'post_content' => $template_content,
+    ));
+
+    // Update form data in post meta
+    store_post_meta($page_id);
+    
+    // Redirect to the updated page
+    wp_redirect(get_permalink($page_id));
+    exit;
+}
+
+function delete_page() {
+    $page_id = intval($_POST['page_id']);
+    wp_delete_post($page_id, true);
+    wp_redirect(admin_url('edit.php?post_type=page'));
+    exit;
+}
+
+function fetch_template_content() {
+    $template_id = intval($_POST['existing_page']);
+    $template_post = get_post($template_id);
+    return $template_post->post_content;
+}
+
+function replace_dynamic_content($template_content) {
+    foreach ($_POST as $key => $value) {
+        if (strpos($key, 'rdynamic_') === 0) {
+            $dynamic_name = substr($key, 9); // Remove 'rdynamic_' prefix
+            $template_content = preg_replace(
+                '/\{\{\{rdynamic_content type=[\'"][^\'"]+[\'"] name=[\'"]' . preg_quote($dynamic_name, '/') . '[\'"] title=[\'"][^\'"]+[\'"]\}\}\}/',
+                wp_kses_post($value),
+                $template_content
+            );
+        }
+    }
+    return $template_content;
+}
+
+function store_post_meta($post_id) {
+    $meta_data = array();
+    foreach ($_POST as $key => $value) {
+        if (strpos($key, 'rdynamic_') === 0) {
+            $meta_data[$key] = wp_kses_post($value);
+        }
+    }
+    // Store additional meta
+    $meta_data['rpage_category'] = sanitize_text_field($_POST['template_title']);
+    $meta_data['rdynamic_template_id'] = intval($_POST['existing_page']);
+    
+    // Store all meta data in a single meta field
+    update_post_meta($post_id, 'rdynamic_meta_data', $meta_data);
+}
+
+// Render form for importing templates
 if (!isset($_POST['import_template'])): ?>
     <form method="post" action="" id="import_template_form" style="display: none;">
         <h2>Choose Template</h2>
-        <input type="hidden" name="child_of_rpages" value="" id="child_of_rpages_input">
         <label for="template_title">Template Title:</label>
         <input type="text" id="template_title" name="template_title" required>
 
@@ -194,6 +143,7 @@ if (!isset($_POST['import_template'])): ?>
         <button type="submit" name="import_template">Import</button>
     </form>
 <?php endif; ?>
+
 <?php if (isset($_POST['import_template'])): ?>
     <?php
     // Fetch the selected template's content
@@ -234,7 +184,11 @@ if (!isset($_POST['import_template'])): ?>
 
         <?php foreach ($matches as $match): ?>
             <label for="<?php echo esc_attr($match[2]); ?>"><?php echo esc_html($match[3]); ?>:</label>
-            <input type="<?php echo esc_attr($match[1]); ?>" id="<?php echo esc_attr($match[2]); ?>" name="rdynamic_<?php echo esc_attr($match[2]); ?>" required value="<?php echo isset($_POST['page_id']) ? esc_attr(get_post_meta($_POST['page_id'], 'rdynamic_' . $match[2], true)) : ''; ?>">
+            <?php if ($match[1] === 'text_area' || $match[1] === 'textarea' || $match[1] === 'Textarea' || $match[1] === 'TextArea'): ?>
+                <textarea id="<?php echo esc_attr($match[2]); ?>" name="rdynamic_<?php echo esc_attr($match[2]); ?>" required><?php echo isset($_POST['page_id']) ? esc_attr(get_post_meta($_POST['page_id'], 'rdynamic_' . $match[2], true)) : ''; ?></textarea>
+            <?php else: ?>
+                <input type="<?php echo esc_attr($match[1]); ?>" id="<?php echo esc_attr($match[2]); ?>" name="rdynamic_<?php echo esc_attr($match[2]); ?>" required value="<?php echo isset($_POST['page_id']) ? esc_attr(get_post_meta($_POST['page_id'], 'rdynamic_' . $match[2], true)) : ''; ?>">
+            <?php endif; ?>
         <?php endforeach; ?>
 
         <?php if ($loop_match): ?>
